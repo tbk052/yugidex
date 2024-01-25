ba36cb13d94f68f7964eb36fd19fb2252c29d6fb
Phạm Đình Thanh Bình <thanhbinhtk052@gmail.com>
2024-01-25 15:02:12 +0700

    init

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import {Text, View} from 'react-native';
+const App = () => {
+  return (
+    <View>
+      <Text>Hello World</Text>
+    </View>
+  );
+};
+
+export default App;
